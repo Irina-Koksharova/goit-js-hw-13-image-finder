18d036435ed0b8fd1d10216fa606bbaf3426ad23
Irina-Koksharova <biodgerelo@ukr.net>
2020-11-07 18:15:14 +0200

    Добавлена разметка, стили, базовый js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import '../node_modules/modern-normalize/modern-normalize.css';
+import './css/styles.css';
+import markupTemplate from './template.hbs';
+
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+    buttonLoad: document.querySelector('[data-action="load-more"]'),
+    imageGallery: document.querySelector('.gallery'),
+}
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+
+function onSearchFormSubmit(e) {
+    e.preventDefault();
+    const KEY = '19018418-5cf416ff9d3b144c810bafa25';
+    const searchQuery = e.currentTarget.elements.query.value;
+    fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${KEY}`)
+    .then(response => response.json())
+    .then(renderMarkup)
+}
+
+function renderMarkup(images) {
+    const markup = markupTemplate(images);
+       refs.imageGallery.insertAdjacentHTML('afterbegin', markup);
+}
+
+
+
+
+
+    
